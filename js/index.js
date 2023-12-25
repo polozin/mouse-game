63456c2c896017adb2fbcf526373ac217ace941e
Constantin <nufnaf@gmail.com>
2023-12-26 01:09:39 +0200

    ok

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function test(){
+   const content = document.querySelector('.content');
+  console.log(`content height  ${content.clientHeight}`);
+  let arr = [];
+}
--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 app.use(express.static(__dirname + '/')); //__dir and not _dir
 const port = 8000; // you can use any port
 app.listen(port);
-console.log('server on ' + port);
+console.log(`server on http://localhost:${port}/`);

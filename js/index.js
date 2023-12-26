c0e70d74cd97a9d9febdbc005976930189e37734
Constantin <nufnaf@gmail.com>
2023-12-26 22:36:10 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
-function test(){
-   const content = document.querySelector('.content');
-  console.log(`content height  ${content.clientHeight}`);
-  let arr = [];
+const holeHeight = 12;
+const holeWidth = 10;
+const holeLineShift = 0.2;
+const startButton = document.querySelector('.btn-start');
+startButton.addEventListener('click', (e) =>{
+    let holeObject = document.querySelector('.hole');
+    fillHole(holeObject);
+})
+function fillHole(holeObject) {
+    let lineWidth = holeWidth;
+    for (let i = holeHeight; i > 0; i--) {
+        const line = document.createElement('span');
+        line.setAttribute('class', 'hole-line');
+        line.setAttribute('style', `width: ${lineWidth}vw`);
+        lineWidth -= holeLineShift;
+        holeObject.appendChild(line);
+    }
+}
+
+function test() {
+    const content = document.querySelector('.content');
+    console.log(`content height  ${content.clientHeight}`);
+    let arr = [];
 }

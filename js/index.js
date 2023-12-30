68775d38622cd55ab5edcfa95d5391517015e2a2
Constantin <nufnaf@gmail.com>
2023-12-30 04:24:42 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-const COUNT_CELLS = 23;
-const DEFAULT_VALUE = 0;
-const CELL_VALUE = 1;
+const COUNT_CELLS = 28;
+const TYPE_CELL = 0;
+const TYPE_HOLE = 1;
 const COUNT_HOLES = 6;
-
-let cells = Array(COUNT_CELLS).fill(DEFAULT_VALUE);
+const MAXIMUM_INCREMENT = 10000;
+let cells =  initArrayCells();
+let holsPositions = [];
 const rootElement = document.querySelector('#cells-box');
 
 function getRandomInt(min, max) {
@@ -14,41 +15,50 @@ function getRandomInt(min, max) {
 
 function initCells() {
     rootElement.innerHTML = '';
-    const holsPositions = [];
-    for (let i = 0; i < COUNT_HOLES; i++) {
+    holsPositions = [];
+    for (let i = 0, increment = 0; i < COUNT_HOLES; i++, increment++) {
         let position = getRandomInt(0, COUNT_CELLS);
-        if (cells[position] === 0 && holsPositions.includes(position) === false) {
+        if (cells[position].type === 0 && holsPositions.includes(position) === false) {
             holsPositions.push(position);
         } else {
             i--;
         }
+        if( increment > MAXIMUM_INCREMENT){
+            throw new Error('Local Error: Overflow Increment ');
+        }
     }
-    console.log("holsPositions", holsPositions);
-    cells = Array(COUNT_CELLS).fill(DEFAULT_VALUE);
+
+    cells = initArrayCells();
 
     for (let i = 0; i < cells.length; i++) {
         let cellElement = document.createElement('div');
         let className = 'cell';
         if (holsPositions.includes(i) === true) {
             className = 'hole';
-            cells[i] = CELL_VALUE;
+            cells[i].type = TYPE_HOLE;
         }
         cellElement.setAttribute('class', className);
         rootElement.appendChild(cellElement);
+        cells[i].offset.left = cellElement.offsetLeft;
+        cells[i].offset.top = cellElement.offsetTop;
+       // console.log('offset', "left", cellElement.offsetLeft, "top", cellElement.offsetTop);
     }
+    let flyElement = document.createElement('img');
+    flyElement.src = '../assets/images/fly.png';
+    flyElement.offsetTop = cells[0].offset.top;
+    flyElement.offsetLeft = cells[0].offsetLeft;
+    flyElement.setAttribute('class', 'fly');
+    rootElement.appendChild(flyElement);
+
+}
+function initArrayCells() {
+    return Array(COUNT_CELLS).fill({type: TYPE_CELL, offset: {left: 0, top: 0}});
 }
 
-function test() {
-    const content = document.querySelector('.content');
-    console.log(`content height  ${content.clientHeight}`);
-    let arr = [];
-}
+
 
 const startButton = document.querySelector('.btn-start');
 startButton.addEventListener('click', (e) => {
     initCells();
-})
+});
 
-function fillHole(holeObject) {
-
-}
